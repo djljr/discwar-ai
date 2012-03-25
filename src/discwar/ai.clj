ac63e2f93004f2296bcd24ecd9f9d783519ef4c9
Den Lipovsky <den@denlipovsky.com>
2012-03-25 18:45:11 -0400

    bunch of improvements to how the ai reacts to the opponent and powerups

--- src/discwar/ai.clj
+++ src/discwar/ai.clj
@@ -18,15 +18,35 @@
 (defn opponent-str [me]
   (if (= me "player0") "player1" "player0"))
 
-(defn filter-type [type coll]
-  (first (filter (fn [x] (= type (get x "type"))) coll)))
+(defn dist [x1 y1 x2 y2]
+  (let [dx (- x2 x1)
+        dy (- y2 y1)]
+    (Math/sqrt (+ (* dx dx) (* dy dy)))))
 
-(defn find-opponent [me all]
+(defn filter-type [type coll x-center y-center]
+  (closest-to-center (filter (fn [x] (= type (get x "type"))) coll) x-center y-center))
+
+(defn closest-to-center [coll x-center y-center]
+  (first (sort (fn [o1 o2] (- (get o1 "dist") (get o2 "dist")))
+    (map (fn [o] (assoc o "dist" (dist(get o "x") (get o "y") x-center y-center))) coll))))
+
+(defn find-opponent [me all x-center y-center]
   (let [opponent-str (opponent-str (get me "type"))]
-    (filter-type opponent-str all)))
+    (filter-type opponent-str all x-center y-center)))
 
-(defn find-powerup [all]
-  (filter-type "powerup" all))
+(defn find-powerup [all x-center y-center]
+  (filter-type "powerup" all x-center y-center))
+
+(defn is-inside [me opp x-center y-center]
+  (let [x-me (get me "x")
+        y-me (get me "y")
+        x-opp (get opp "x")
+        y-opp (get opp "y")
+        dist-me (dist x-me y-me x-center y-center)
+        dist-opp (dist x-opp y-opp x-center y-center)]
+    (debug "center: " x-center ", " y-center "; dist-me: " dist-me ", dist-opp: " dist-opp)
+    (< dist-me dist-opp)))
+      
 
 (defn find-mass-diff [me opp]
   (let [my-mass (get me "mass")
@@ -37,12 +57,21 @@
   "A wrapper to log before returning the final theta value"
   th)
 
+(defn fast-move-to-goal [th-goal th-delta]
+  (debug "using fast fn")
+  (+ th-goal th-delta))
+
 (defn compute-acceleration-theta [a-max th-current th-goal]
   (let [th-delta (- th-goal th-current)
         pi-over-2 (/ Math/PI 2)]
+    (debug "th-delta: " th-delta ", th-goal: " th-goal ", th-current: " th-current)
     (if 
-      (or (> th-delta pi-over-2) (< th-delta pi-over-2)) th-goal
-      (+ th-goal th-delta))))
+      (or (> th-delta pi-over-2) (< th-delta (- 0 pi-over-2))) th-goal
+      (fast-move-to-goal th-goal th-delta))))
+
+(defn red []
+  (debug "red zone occupied")
+  :red)
 
 (defn compute-zone [obj settings]
   (let [x-obj (get obj "x")
@@ -50,12 +79,10 @@
         x-center (/ (get settings "maxWidth") 2)
         y-center (/ (get settings "maxHeight") 2)
         radius (get settings "boardRadius")
-        dx (- x-obj x-center)
-        dy (- y-obj y-center)
-        dist-obj (Math/sqrt (+ (* dx dx) (* dy dy)))]
+        dist-obj (dist x-obj y-obj x-center y-center)]
     (cond
-      (> dist-obj (* radius 0.9)) :red
-      (> dist-obj (* radius 0.8)) :yellow
+      (> dist-obj (* radius 0.8)) (red)
+      (> dist-obj (* radius 0.7)) :yellow
       (> dist-obj 0) :green)))
 
 (defn angle-between-points [x1 y1 x2 y2]
@@ -83,7 +110,9 @@
 
 (defn powerup-ai [me all settings]
   (let [max-acc (get me "maxAcc")
-        powerup (find-powerup all)
+        x-center (/ (get settings "maxWidth") 2) 
+        y-center (/ (get settings "maxHeight") 2)
+        powerup (find-powerup all x-center y-center)
         v-current (get me "v")
         v-th-current (get v-current "th")
         x-me (get me "x")
@@ -95,7 +124,9 @@
 
 (defn aggressive-ai [me all settings]
   (let [max-acc (get me "maxAcc")
-        opponent (find-opponent me all)
+        x-center (/ (get settings "maxWidth") 2) 
+        y-center (/ (get settings "maxHeight") 2)
+        opponent (find-opponent me all x-center y-center)
         v-current (get me "v")
         v-th-current (get v-current "th")
         x-me (get me "x")
@@ -105,18 +136,38 @@
         th-goal (angle-between-points x-me y-me x-goal y-goal)]
     (gen-response max-acc (compute-acceleration-theta max-acc v-th-current th-goal))))
 
+(defn aggressive-ai-when-hes-on-the-ropes [me all settings]
+  (info "choosing aggressive-ai because we have them on the ropes")
+  (aggressive-ai me all settings))
+
+(defn center-ai-when-in-yellow-zone [me all settings]
+  (info "choosing center-ai due to yellow or red zone infraction while outside of opponent")
+  (center-ai me all settings))
+
+(defn center-ai-when-behind-in-power [me all settings]
+  (info "choosing center-ai due to power difference")
+  (center-ai me all settings))
+
+(defn powerup-ai-when-in-range [me all settings]
+  (info "choosing powerup-ai since there is one in range")
+  (powerup-ai me all settings))
+
 (defn choose-ai [me all settings]
   (let [max-acc (get me "maxAcc")
-        opponent (find-opponent me all)
-        powerup (find-powerup all)
+        x-center (/ (get settings "maxWidth") 2) 
+        y-center (/ (get settings "maxHeight") 2)
+        opponent (find-opponent me all x-center y-center)
+        powerup (find-powerup all x-center y-center)
         mass-diff (find-mass-diff me opponent)
         zone-me (compute-zone me settings)
-        zone-opponent (compute-zone opponent settings)]
+        zone-opponent (compute-zone opponent settings)
+        me-inside (is-inside me opponent x-center y-center)]
     (cond
-      (and (not (= nil powerup)) (= :green (compute-zone powerup settings)) 
-        (or (< mass-diff 1) (and (= :green zone-me) (= :green zone-opponent)))) (powerup-ai me all settings)
-      (< mass-diff -0.6) (center-ai me all settings)
-      (and (= :red zone-me) (not (= :red zone-opponent))) (center-ai me all settings)
+      (and (= :yellow zone-opponent) me-inside) (aggressive-ai-when-hes-on-the-ropes me all settings)
+      (and (not (= nil powerup)) (not (= :red (compute-zone powerup settings))) 
+        (or (< mass-diff 1) (and (= :green zone-me) (= :green zone-opponent)))) (powerup-ai-when-in-range me all settings)
+      (< mass-diff -0.6) (center-ai-when-behind-in-power me all settings)
+      (and (not (= :green zone-me)) (not me-inside)) (center-ai-when-in-yellow-zone me all settings)
       (and) (aggressive-ai me all settings))))
 
 (defn ai-response [ai params]
